e684c0ce89a96a2a05b9fa4f5af7d5850ab97a90
Martin Coronel <coronelmartin@hotmail.cl>
2026-05-21 17:17:39 -0400

    fix: se cambia el puerto linea:225

--- app.py
+++ app.py
@@ -222,4 +222,4 @@ if __name__ == '__main__':
     print(f'URL del backend: {BACKEND_URL}')
     
     # Iniciar el servidor Flask
-    app.run(host='0.0.0.0', port=port, debug=debug_mode)
+    app.run(host='0.0.0.0', port=5000, debug=debug_mode)
